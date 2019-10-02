8de85b25794779dc5e99b42f11c7f22657274e50
ruslav <mateusz.gajo9@wp.pl>
2019-10-02 19:49:50 +0200

    changed name style file, removed unuse code

--- components/MultiStepForm/__tests__/MultiStepForm.test.js
+++ components/MultiStepForm/__tests__/MultiStepForm.test.js
@@ -1,10 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import { MultiStepFormContext } from "../MultiStepFormContext";
 import * as MultiStepForm from "../MultiStepForm";
 
 describe("MultiStepForm components", () => {
-  it("dispalys form buttons", () => {
-    const { getByText } = render(<MultiStepForm.Controls />);
-  });
+  
 });
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,11 @@
 import React, { useContext } from "react";
 import HomePage from "../components/Layout/HomePage";
-import Router from "next/router";
 import "bulma";
-import "./index.module.scss";
+import "./index.scss";
 import './reset.scss'
 
 const Home = () => <HomePage />;
 
-Home.getInitialProps = async function({ res }) {
-  {
-    // if (res) {
-    //   res.writeHead(302, {
-    //     Location: "/signup"
-    //   });
-    //   res.end();
-    // } else {
-    //   Router.push("/signup");
-    // }
-    return {};
-  }
-};
+
 
 export default Home;
