dda3a1cb73c0bc79e4f8bf93c2950ed0e280a4d6
Mateusz Gajo <mateusz.gajo9@wp.pl>
2019-12-28 07:58:27 +0100

    Added SEO

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from 'next/head';
 import HomePage from "../features/components/Layout/HomePage";
 import { isLogged, getSession } from "../helper";
 import "bulma";
@@ -8,6 +9,28 @@ import "./styles/customize.scss";
 
 const Home = ({ isSession }) => {
   return (
+    <>
+    <Head>
+    <meta
+      name="description"
+      content="Bringo, strona do nauki słówek. Ucz się codziennie angielskich słówek."
+    />
+    <meta name="author" content="Mateusz Gajo" />
+    <meta
+      name="keywords"
+      content="Bringo, nauka słówek, angielskie słówka, strona do nauki"
+    />
+    <meta name="url" content="https://bringo-app.herokuapp.com/" />
+    <meta property="og:title" content="Bringo" />
+    <meta
+      property="og:image"
+      content="https://raw.githubusercontent.com/MateuszGajo/Portfolio/master/assets/bringo.jpg"
+    />
+    <meta
+      property="og:description"
+      content="Strona do nauki słówek"
+    />
+    </Head>
     <HomePage>
       <div className="menu">
         <a className="button is-fullwidth" href="/learning">
@@ -18,6 +41,7 @@ const Home = ({ isSession }) => {
         </a>
       </div>
     </HomePage>
+    </>
   );
 };
 
